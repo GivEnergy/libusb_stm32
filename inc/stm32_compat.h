1671610ada87199785fcf4ed6f65e7bea00b2323
Aidan MacDonald <aidanmacdonald@codethink.co.uk>
2025-08-28 15:32:51 +0100

    Hardcode STM32F103 variant in stm32_compat.h

--- inc/stm32_compat.h
+++ inc/stm32_compat.h
@@ -1,11 +1,10 @@
+// MODIFIED BY GIVENERGY
+
 #ifndef _STM32_COMPAT_H_
 #define _STM32_COMPAT_H_
 
-#ifndef PLATFORMIO
+#include "stm32f103xb.h"
 
-#include <stm32.h>
-
-#else // PLATFORMIO
 /* modify bitfield */
 #define _BMD(reg, msk, val)     (reg) = (((reg) & ~(msk)) | (val))
 /* set bitfield */
@@ -20,40 +19,5 @@
 #define _WVL(reg, msk, val)     while(((reg) & (msk)) != (val))
 /* bit value */
 #define _BV(bit)                (0x01 << (bit))
-
-#if defined(STM32F0xx)
-    #include <stm32f0xx.h>
-#elif defined(STM32F1xx)
-    #include <stm32f1xx.h>
-#elif defined(STM32F2xx)
-    #include <stm32f2xx.h>
-#elif defined(STM32F3xx)
-    #include <stm32f3xx.h>
-#elif defined(STM32F4xx)
-    #include <stm32f4xx.h>
-#elif defined(STM32F7xx)
-    #include <stm32f7xx.h>
-#elif defined(STM32H7xx)
-    #include <stm32h7xx.h>
-#elif defined(STM32L0xx)
-    #include <stm32l0xx.h>
-#elif defined(STM32L1xx)
-    #include <stm32l1xx.h>
-#elif defined(STM32L4xx)
-    #include <stm32l4xx.h>
-#elif defined(STM32L5xx)
-    #include <stm32l5xx.h>
-#elif defined(STM32G0xx)
-    #include <stm32g0xx.h>
-#elif defined(STM32G4xx)
-    #include <stm32g4xx.h>
-#elif defined(STM32WBxx)
-    #include <stm32wbxx.h>
-#else
-    #error "STM32 family not defined"
-#endif
-
-#endif // PLATFORMIO
-
 
 #endif // _STM32_COMPAT_H_
